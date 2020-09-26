5c1d2b946a8348cb621a8d8f61c1bef5893d08d8
Alex-Serky <aserky@gmail.com>
2020-09-26 22:33:02 +0200

    Document Object Model DOM et Calculs

---
+++ 2-Moyen/7-exo7 : DOM et Calculs/programme/calcul.js
@@ -0,0 +1,18 @@
+var calculManager = {
+
+    addition : function(chiffre){
+        var resultat = 0;
+        for (var i = 1; i <= chiffre; i++) {
+            resultat += i;
+        }
+        return resultat;
+    },
+
+    factorielle : function (chiffre) {
+        var resultat = 1;
+        for (var i = 1; i <= chiffre; i++) {
+            resultat *= i;
+        }
+        return resultat;
+    }
+};
