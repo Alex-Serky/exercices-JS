745ab4a65533fc395e37ad2fcc5bc510b6cac85d
Alex-Serky <aserky@gmail.com>
2020-09-26 22:06:12 +0200

    Boucle For et Debugging

---
+++ 1-Facile/18-exo18 : Boucle For et debugging/main.js
@@ -0,0 +1,9 @@
+var addition = 0;
+
+for (var i = 1; i <= 10; i++) {
+
+    addition += i;
+    console.log("Etape " + i + " : " + addition);
+}
+
+console.log("Le résultat des 10 premiers chiffres est égal à " + " : " + addition);
